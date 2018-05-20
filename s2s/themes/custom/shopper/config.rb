ff698b447bfde84d1756114c31ed267f6d198470
sakthivel <sakthivel@valuebound.com>
2018-05-20 06:54:58 +0530

    s2s theme created

---
+++ s2s/themes/custom/shopper/config.rb
@@ -0,0 +1,15 @@
+
+# We tell compass that we want to use. SCSS format in this case
+preferred_syntax = :scss
+
+# Where we want our CSS files to go
+css_dir = 'css'
+
+# Where we want to put our source SASS Files
+sass_dir = 'sass'
+
+# Where we want to put our source JS Files
+js_dir = 'js'
+
+# While in Dev, we can use this feature to easily debug the CSS styles
+line_comments = true
